34267faf79eebf4d3957a38e8580384d9e46f8ba
ErlanDjamankulov <erlan.djamankulov@gmail.com>
2024-06-06 22:35:45 +0600

    123

--- wedding_invite/settings.py
+++ wedding_invite/settings.py
@@ -90,25 +90,25 @@ WSGI_APPLICATION = 'wedding_invite.wsgi.application'
 
 
 
-# if DOCKER_STARTUP:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": os.getenv("POSTGRES_DB"),
-#             "USER": os.getenv("POSTGRES_USER"),
-#             "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-#             "HOST": os.getenv("POSTGRES_HOST"),
-#             "PORT": os.getenv("POSTGRES_PORT"),
-#         }
-#     }
-#
-# else:
-DATABASES = {
+if DOCKER_STARTUP:
+    DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("POSTGRES_HOST"),
+            "PORT": os.getenv("POSTGRES_PORT"),
         }
     }
+
+else:
+    DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR / "db.sqlite3",
+            }
+        }
 
 
 # Password validation
